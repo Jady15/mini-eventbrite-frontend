4e76350248b744ff882f6bbc36d3ae32393c273f
Isaac Luviano <isaacluvianohernandez@gmail.com>
2025-09-02 19:07:59 -0700

    feat: Se agrega funcionalidad para creación de eventos

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Routes, Route, useLocation, useNavigate } from 'react-router-dom'
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import Navbar from './components/Navbar'
 import Login from './pages/Login'
 import Register from './pages/Register'
+import CreateEvent from './pages/CreateEvent'
 import NotFound from './pages/NotFound'
+import ProtectedRoute from './components/ProtectedRoute'
 import useAuth from './hooks/useAuth'
 import useLastVisited from './hooks/useLastVisited'
 
@@ -22,6 +24,8 @@ export default function App() {
     }
   }, [isAuthenticated, getLastPath, navigate])
 
+  const hasRole = (...roles) => roles.includes(user?.role)
+
   return (
     <div className='min-h-screen'>
       <Navbar />
@@ -29,7 +33,15 @@ export default function App() {
               <Routes>
                 <Route path='/' element={<Login/>} />
                 <Route path='/register' element={<Register/>} />
-                <Route path='*' element={<NotFound/>}></Route>
+                <Route
+                 path='/events/new' 
+                 element={ 
+                  <ProtectedRoute>
+                    { hasRole('organizer','admin') ? <CreateEvent /> : <NotFound />}
+                  </ProtectedRoute>
+                }
+                />
+                <Route path='*' element={<NotFound/>} />
               </Routes>
       </main>
     </div>
